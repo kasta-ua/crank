041b7657367d35db2988efb0cfe23c2c38464ab8
Alexander Solovyov <alexander@solovyov.net>
2018-01-05 14:23:57 +0200

    naive exception handling

--- src/crank/worker.clj
+++ src/crank/worker.clj
@@ -29,13 +29,18 @@
   Runnable
   (run [this]
     (let [func (:func data)]
-      (loop [messages (input/receive input 100)]
-        (doseq [message messages]
-          (log/debug "processing message" message)
-          (func message)
-          (input/ack input message))
-        (when-not @stop
-          (recur (input/receive input 100)))))))
+      (try
+        (loop [messages (input/receive input 100)]
+          (doseq [message messages]
+            (log/debug "processing message" message)
+            (func message)
+            (input/ack input message))
+          (when-not @stop
+            (recur (input/receive input 100))))
+        (catch Exception e
+          (log/error e)
+          ;; FIXME: properly stop job and exit here
+          :exit)))))
 
 
 ;; TODO: check jobs on valid format
